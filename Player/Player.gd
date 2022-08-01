625fda297646c380c5defb31faa941bf321c9a48
bluej3270 <kellanclapp@gmail.com>
2022-08-01 15:37:08 -0600

    Player exported an added, camera stuff

---
+++ Player/Player.gd
@@ -0,0 +1,85 @@
+extends KinematicBody
+
+var gravity = -30
+var max_speed = 6
+
+var mouse_sensitivity = 15
+var min_look_angle = -20
+var max_look_angle = 75
+var mouse_delta = Vector2()
+onready var pivot = get_node("Pivot")
+
+var velocity = Vector3()
+
+var time_from_on_floor : float
+var coyote_time = 0.25
+
+var jump = 10
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func get_input():
+	var input_dir = Vector3()
+	# desired move in camera direction
+	if Input.is_action_pressed("forward"):
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("backward"):
+		input_dir += global_transform.basis.z
+	if Input.is_action_pressed("left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("right"):
+		input_dir += global_transform.basis.x
+	input_dir = input_dir.normalized()
+	return input_dir
+
+#func _unhandled_input(event):
+#	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+#		rotate_y(-event.relative.x * mouse_sensitivity)
+#		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+#		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.is_action_pressed("rclick"):
+		mouse_delta = event.relative
+
+func _process(delta):
+	var rot = Vector3(mouse_delta.y, mouse_delta.x, 0) * mouse_sensitivity * delta
+	
+	pivot.rotation_degrees.x += rot.x
+	print(pivot.rotation_degrees.x)
+	print(new_clamp(rotation_degrees.x, min_look_angle, max_look_angle))
+	pivot.rotation_degrees.x = new_clamp(rotation_degrees.x, min_look_angle, max_look_angle)
+
+	
+	pivot.rotation_degrees.y += rot.y
+	
+	mouse_delta = Vector2()
+
+func _physics_process(delta):
+	if self.is_on_floor():
+		time_from_on_floor = 0
+	else: 
+		time_from_on_floor += 1*delta
+	
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	
+	if Input.is_action_just_pressed("jump"):
+		if time_from_on_floor <= coyote_time:
+			velocity.y = jump
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+
+func new_clamp(value, min_val, max_val):
+	if value <= min_val:
+		print('Min-Val')
+		return min_val
+	elif value >= max_val:
+		print('Max-Val')
+		return max_val
+	else:
+		print('OG Val')
+		return value
---
+++ World/Multi-Mesh.gd
@@ -0,0 +1,25 @@
+extends MultiMeshInstance
+
+func _ready():
+	var multimesh = get_node('.').multimesh
+	
+	for index in multimesh.instance_count:
+		var collision_shape = CollisionShape.new()
+		var mesh_tran = multimesh.get_instance_transform(index)
+		collision_shape.shape = multimesh.mesh.create_trimesh_shape()
+		
+		collision_shape.transform.basis.x = mesh_tran.basis.x
+		collision_shape.transform.basis.z = mesh_tran.basis.z
+		collision_shape.transform.basis.y = mesh_tran.basis.y
+		collision_shape.scale = Vector3(1, 1, 1)
+		
+		collision_shape.rotation.y = 25.132
+		collision_shape.rotation.z = 25.132
+		collision_shape.rotation.x = 25.132
+		
+		var static_body = StaticBody.new()
+		static_body.transform = mesh_tran
+		static_body.add_child(collision_shape)
+		self.add_child(static_body)
+		
+		index += 1
